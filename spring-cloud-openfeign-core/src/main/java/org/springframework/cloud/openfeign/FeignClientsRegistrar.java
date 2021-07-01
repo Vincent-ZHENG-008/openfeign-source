765b3f0394759fbb321e2da7e9e6f9553abeeb32
开发-郑文焕 <zhengwenhuan@gdmcmc.cn>
2021-07-01 16:02:55 +0800

    init

---
+++ spring-cloud-openfeign-core/src/main/java/org/springframework/cloud/openfeign/FeignClientsRegistrar.java
@@ -0,0 +1,473 @@
+/*
+ * Copyright 2013-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.openfeign;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import feign.Request;
+
+import org.springframework.aop.scope.ScopedProxyUtils;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.BeanExpressionContext;
+import org.springframework.beans.factory.config.BeanExpressionResolver;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author Spencer Gibb
+ * @author Jakub Narloch
+ * @author Venil Noronha
+ * @author Gang Li
+ * @author Michal Domagala
+ * @author Marcin Grzejszczak
+ * @author Olga Maciaszek-Sharma
+ * @author Jasbir Singh
+ */
+class FeignClientsRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware, EnvironmentAware {
+
+	// patterned after Spring Integration IntegrationComponentScanRegistrar
+	// and RibbonClientsConfigurationRegistgrar
+
+	private ResourceLoader resourceLoader;
+
+	private Environment environment;
+
+	FeignClientsRegistrar() {
+	}
+
+	static void validateFallback(final Class clazz) {
+		Assert.isTrue(!clazz.isInterface(), "Fallback class must implement the interface annotated by @FeignClient");
+	}
+
+	static void validateFallbackFactory(final Class clazz) {
+		Assert.isTrue(!clazz.isInterface(), "Fallback factory must produce instances "
+				+ "of fallback classes that implement the interface annotated by @FeignClient");
+	}
+
+	static String getName(String name) {
+		if (!StringUtils.hasText(name)) {
+			return "";
+		}
+
+		String host = null;
+		try {
+			String url;
+			if (!name.startsWith("http://") && !name.startsWith("https://")) {
+				url = "http://" + name;
+			}
+			else {
+				url = name;
+			}
+			host = new URI(url).getHost();
+
+		}
+		catch (URISyntaxException e) {
+		}
+		Assert.state(host != null, "Service id not legal hostname (" + name + ")");
+		return name;
+	}
+
+	static String getUrl(String url) {
+		if (StringUtils.hasText(url) && !(url.startsWith("#{") && url.contains("}"))) {
+			if (!url.contains("://")) {
+				url = "http://" + url;
+			}
+			try {
+				new URL(url);
+			}
+			catch (MalformedURLException e) {
+				throw new IllegalArgumentException(url + " is malformed", e);
+			}
+		}
+		return url;
+	}
+
+	static String getPath(String path) {
+		if (StringUtils.hasText(path)) {
+			path = path.trim();
+			if (!path.startsWith("/")) {
+				path = "/" + path;
+			}
+			if (path.endsWith("/")) {
+				path = path.substring(0, path.length() - 1);
+			}
+		}
+		return path;
+	}
+
+	@Override
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
+	@Override
+	public void registerBeanDefinitions(AnnotationMetadata metadata, BeanDefinitionRegistry registry) {
+		/* 注册全局默认的 FeignClient 配置组件 */
+		registerDefaultConfiguration(metadata, registry);
+		/* 注册 @FeignClient 保准接口 */
+		registerFeignClients(metadata, registry);
+	}
+
+	private void registerDefaultConfiguration(AnnotationMetadata metadata, BeanDefinitionRegistry registry) {
+		Map<String, Object> defaultAttrs = metadata.getAnnotationAttributes(EnableFeignClients.class.getName(), true);
+
+		if (defaultAttrs != null && defaultAttrs.containsKey("defaultConfiguration")) {
+			String name;
+			if (metadata.hasEnclosingClass()) {
+				name = "default." + metadata.getEnclosingClassName();
+			}
+			else {
+				name = "default." + metadata.getClassName();
+			}
+			registerClientConfiguration(registry, name, defaultAttrs.get("defaultConfiguration"));
+		}
+	}
+
+	public void registerFeignClients(AnnotationMetadata metadata, BeanDefinitionRegistry registry) {
+		/* 被 @FeignClient 标注可以注册为 SpringBean 的集合 */
+		LinkedHashSet<BeanDefinition> candidateComponents = new LinkedHashSet<>();
+		/* 获取 @EnableFeignClients 属性的 clients 属性 */
+		Map<String, Object> attrs = metadata.getAnnotationAttributes(EnableFeignClients.class.getName());
+		final Class<?>[] clients = attrs == null ? null : (Class<?>[]) attrs.get("clients");
+		if (clients == null || clients.length == 0) {
+			/* 获取根据 classpath 路径进行扫描的 Provider 类 */
+			ClassPathScanningCandidateComponentProvider scanner = getScanner();
+			scanner.setResourceLoader(this.resourceLoader);
+			/* 扫描被 @FeignClient 注解修饰的接口 */
+			scanner.addIncludeFilter(new AnnotationTypeFilter(FeignClient.class));
+			/* 获取扫描包集合 */
+			Set<String> basePackages = getBasePackages(metadata);
+			for (String basePackage : basePackages) {
+				candidateComponents.addAll(scanner.findCandidateComponents(basePackage));
+			}
+		}
+		else {
+			/* 把 @EnableFeignClients 的 clients 属性 class数组 添加到 candidateComponents 集合中 */
+			for (Class<?> clazz : clients) {
+				candidateComponents.add(new AnnotatedGenericBeanDefinition(clazz));
+			}
+		}
+
+		/* 循环注册 FeignClient 到 SpringBeanDefinition 中 */
+		for (BeanDefinition candidateComponent : candidateComponents) {
+			if (candidateComponent instanceof AnnotatedBeanDefinition) {
+				// verify annotated class is an interface
+				AnnotatedBeanDefinition beanDefinition = (AnnotatedBeanDefinition) candidateComponent;
+				AnnotationMetadata annotationMetadata = beanDefinition.getMetadata();
+				Assert.isTrue(annotationMetadata.isInterface(), "@FeignClient can only be specified on an interface");
+
+				/* 获取接口上面 @FeignClient 标注的注解信息 */
+				Map<String, Object> attributes = annotationMetadata
+						.getAnnotationAttributes(FeignClient.class.getCanonicalName());
+				/* 获取 @FeignClient 以 contextId->value->name->serviceId 进行逐步获取 feign 名称  */
+				String name = getClientName(attributes);
+				/* 注册当前 FeignClient 的配置类 */
+				registerClientConfiguration(registry, name, attributes.get("configuration"));
+				/* 注册 FeignClient 到 Spring 容器中 */
+				registerFeignClient(registry, annotationMetadata, attributes);
+			}
+		}
+	}
+
+	private void registerFeignClient(BeanDefinitionRegistry registry, AnnotationMetadata annotationMetadata,
+			Map<String, Object> attributes) {
+		/* 获取 FeignClient 接口类名称 */
+		String className = annotationMetadata.getClassName();
+		/* 反射获取 FeignClient 接口类 */
+		Class clazz = ClassUtils.resolveClassName(className, null);
+		/* 获取 Spring ApplicationContext BeanFactory */
+		ConfigurableBeanFactory beanFactory = registry instanceof ConfigurableBeanFactory
+				? (ConfigurableBeanFactory) registry : null;
+		String contextId = getContextId(beanFactory, attributes);
+		String name = getName(attributes);
+		/* 创建 FeignClientBean */
+		FeignClientFactoryBean factoryBean = new FeignClientFactoryBean();
+		factoryBean.setBeanFactory(beanFactory);
+		factoryBean.setName(name);
+		factoryBean.setContextId(contextId);
+		factoryBean.setType(clazz);
+		/* @RefreshScope 支持 */
+		factoryBean.setRefreshableClient(isClientRefreshEnabled());
+		/* 构建 BeanDefinitionBuilder */
+		BeanDefinitionBuilder definition = BeanDefinitionBuilder.genericBeanDefinition(clazz, () -> {
+			/* 设置当前 Feign 客户端的请求URL地址 */
+			factoryBean.setUrl(getUrl(beanFactory, attributes));
+			/* 设置当前 Feign 客户端的请求地址前缀 */
+			factoryBean.setPath(getPath(beanFactory, attributes));
+			/* 设置当前 Feign 客户端请求404时，是解析请求还是抛出异常，默认是 false */
+			factoryBean.setDecode404(Boolean.parseBoolean(String.valueOf(attributes.get("decode404"))));
+			/* 判断当前 Feign 客户端是否有降级处理 Bean 对象；如果有就设置指定类对象，且该对象必须存在于 Spring-IOC 容器中 */
+			Object fallback = attributes.get("fallback");
+			if (fallback != null) {
+				factoryBean.setFallback(fallback instanceof Class ? (Class<?>) fallback
+						: ClassUtils.resolveClassName(fallback.toString(), null));
+			}
+			/* 判断当前 Feign 客户端是否有返回处理对象工厂，该工厂必须要生成返回处理对象，也就是必须存在于 Spring-IOC 容器中 */
+			Object fallbackFactory = attributes.get("fallbackFactory");
+			if (fallbackFactory != null) {
+				factoryBean.setFallbackFactory(fallbackFactory instanceof Class ? (Class<?>) fallbackFactory
+						: ClassUtils.resolveClassName(fallbackFactory.toString(), null));
+			}
+			/* 获取 FeignClientFactoryBean 动态代理对象 */
+			return factoryBean.getObject();
+		});
+		/* 设置按类型自动注入 */
+		definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+		/* 设置懒加载 */
+		definition.setLazyInit(true);
+		validate(attributes);
+
+		/* 通过 BeanDefinitionBuilder 获取 BeanDefinition */
+		AbstractBeanDefinition beanDefinition = definition.getBeanDefinition();
+		/* 设置当前的 BeanDefinition 注入类型是接口注入 */
+		beanDefinition.setAttribute(FactoryBean.OBJECT_TYPE_ATTRIBUTE, className);
+		/* 设置 FeignClientsRegistrarFactory 其根本是 FeignClientFactoryBean 对象 */
+		beanDefinition.setAttribute("feignClientsRegistrarFactoryBean", factoryBean);
+
+		// has a default, won't be null
+		/* 判断是否设置了优先注入 */
+		boolean primary = (Boolean) attributes.get("primary");
+		beanDefinition.setPrimary(primary);
+
+		/* 判断 Feign 是否有别名，默认为空 */
+		String[] qualifiers = getQualifiers(attributes);
+		if (ObjectUtils.isEmpty(qualifiers)) {
+			qualifiers = new String[] { contextId + "FeignClient" };
+		}
+
+		/* 创建一个带有名称与别名的 Holder，其本身还是 BeanDefinition */
+		BeanDefinitionHolder holder = new BeanDefinitionHolder(beanDefinition, className, qualifiers);
+		/* 动态注册 BeanDefinition */
+		BeanDefinitionReaderUtils.registerBeanDefinition(holder, registry);
+
+		/* 注册一个 RefreshScope，默认是不创建 */
+		registerOptionsBeanDefinition(registry, contextId);
+	}
+
+	private void validate(Map<String, Object> attributes) {
+		AnnotationAttributes annotation = AnnotationAttributes.fromMap(attributes);
+		// This blows up if an aliased property is overspecified
+		// FIXME annotation.getAliasedString("name", FeignClient.class, null);
+		validateFallback(annotation.getClass("fallback"));
+		validateFallbackFactory(annotation.getClass("fallbackFactory"));
+	}
+
+	/* for testing */ String getName(Map<String, Object> attributes) {
+		return getName(null, attributes);
+	}
+
+	String getName(ConfigurableBeanFactory beanFactory, Map<String, Object> attributes) {
+		String name = (String) attributes.get("serviceId");
+		if (!StringUtils.hasText(name)) {
+			name = (String) attributes.get("name");
+		}
+		if (!StringUtils.hasText(name)) {
+			name = (String) attributes.get("value");
+		}
+		name = resolve(beanFactory, name);
+		return getName(name);
+	}
+
+	private String getContextId(ConfigurableBeanFactory beanFactory, Map<String, Object> attributes) {
+		String contextId = (String) attributes.get("contextId");
+		if (!StringUtils.hasText(contextId)) {
+			return getName(attributes);
+		}
+
+		contextId = resolve(beanFactory, contextId);
+		return getName(contextId);
+	}
+
+	private String resolve(ConfigurableBeanFactory beanFactory, String value) {
+		if (StringUtils.hasText(value)) {
+			if (beanFactory == null) {
+				return this.environment.resolvePlaceholders(value);
+			}
+			BeanExpressionResolver resolver = beanFactory.getBeanExpressionResolver();
+			String resolved = beanFactory.resolveEmbeddedValue(value);
+			if (resolver == null) {
+				return resolved;
+			}
+			return String.valueOf(resolver.evaluate(resolved, new BeanExpressionContext(beanFactory, null)));
+		}
+		return value;
+	}
+
+	private String getUrl(ConfigurableBeanFactory beanFactory, Map<String, Object> attributes) {
+		String url = resolve(beanFactory, (String) attributes.get("url"));
+		return getUrl(url);
+	}
+
+	private String getPath(ConfigurableBeanFactory beanFactory, Map<String, Object> attributes) {
+		String path = resolve(beanFactory, (String) attributes.get("path"));
+		return getPath(path);
+	}
+
+	protected ClassPathScanningCandidateComponentProvider getScanner() {
+		return new ClassPathScanningCandidateComponentProvider(false, this.environment) {
+			@Override
+			protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+				boolean isCandidate = false;
+				if (beanDefinition.getMetadata().isIndependent()) {
+					if (!beanDefinition.getMetadata().isAnnotation()) {
+						isCandidate = true;
+					}
+				}
+				return isCandidate;
+			}
+		};
+	}
+
+	protected Set<String> getBasePackages(AnnotationMetadata importingClassMetadata) {
+		Map<String, Object> attributes = importingClassMetadata
+				.getAnnotationAttributes(EnableFeignClients.class.getCanonicalName());
+
+		Set<String> basePackages = new HashSet<>();
+		for (String pkg : (String[]) attributes.get("value")) {
+			if (StringUtils.hasText(pkg)) {
+				basePackages.add(pkg);
+			}
+		}
+		for (String pkg : (String[]) attributes.get("basePackages")) {
+			if (StringUtils.hasText(pkg)) {
+				basePackages.add(pkg);
+			}
+		}
+		for (Class<?> clazz : (Class[]) attributes.get("basePackageClasses")) {
+			basePackages.add(ClassUtils.getPackageName(clazz));
+		}
+
+		if (basePackages.isEmpty()) {
+			basePackages.add(ClassUtils.getPackageName(importingClassMetadata.getClassName()));
+		}
+		return basePackages;
+	}
+
+	private String getQualifier(Map<String, Object> client) {
+		if (client == null) {
+			return null;
+		}
+		String qualifier = (String) client.get("qualifier");
+		if (StringUtils.hasText(qualifier)) {
+			return qualifier;
+		}
+		return null;
+	}
+
+	private String[] getQualifiers(Map<String, Object> client) {
+		if (client == null) {
+			return null;
+		}
+		List<String> qualifierList = new ArrayList<>(Arrays.asList((String[]) client.get("qualifiers")));
+		qualifierList.removeIf(qualifier -> !StringUtils.hasText(qualifier));
+		if (qualifierList.isEmpty() && getQualifier(client) != null) {
+			qualifierList = Collections.singletonList(getQualifier(client));
+		}
+		return !qualifierList.isEmpty() ? qualifierList.toArray(new String[0]) : null;
+	}
+
+	private String getClientName(Map<String, Object> client) {
+		if (client == null) {
+			return null;
+		}
+		String value = (String) client.get("contextId");
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("value");
+		}
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("name");
+		}
+		if (!StringUtils.hasText(value)) {
+			value = (String) client.get("serviceId");
+		}
+		if (StringUtils.hasText(value)) {
+			return value;
+		}
+
+		throw new IllegalStateException(
+				"Either 'name' or 'value' must be provided in @" + FeignClient.class.getSimpleName());
+	}
+
+	private void registerClientConfiguration(BeanDefinitionRegistry registry, Object name, Object configuration) {
+		BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(FeignClientSpecification.class);
+		builder.addConstructorArgValue(name);
+		builder.addConstructorArgValue(configuration);
+		registry.registerBeanDefinition(name + "." + FeignClientSpecification.class.getSimpleName(),
+				builder.getBeanDefinition());
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
+
+	/**
+	 * This method is meant to create {@link Request.Options} beans definition with
+	 * refreshScope.
+	 * @param registry spring bean definition registry
+	 * @param contextId name of feign client
+	 */
+	private void registerOptionsBeanDefinition(BeanDefinitionRegistry registry, String contextId) {
+		if (isClientRefreshEnabled()) {
+			String beanName = Request.Options.class.getCanonicalName() + "-" + contextId;
+			BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder
+					.genericBeanDefinition(OptionsFactoryBean.class);
+			definitionBuilder.setScope("refresh");
+			definitionBuilder.addPropertyValue("contextId", contextId);
+			BeanDefinitionHolder definitionHolder = new BeanDefinitionHolder(definitionBuilder.getBeanDefinition(),
+					beanName);
+			definitionHolder = ScopedProxyUtils.createScopedProxy(definitionHolder, registry, true);
+			BeanDefinitionReaderUtils.registerBeanDefinition(definitionHolder, registry);
+		}
+	}
+
+	private boolean isClientRefreshEnabled() {
+		return environment.getProperty("feign.client.refresh-enabled", Boolean.class, false);
+	}
+
+}
